33947538398a3ddf7c5b469c1e96e4a9b9ecfdee
LwaziMashiza <lwaziluthuli0@gmail.com>
2024-01-25 14:54:01 -0800

    new changes

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,8 @@
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
 # Restart Nginx
 exec { 'nginx-restart':
   command => 'nginx restart',
